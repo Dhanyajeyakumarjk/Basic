0569cbd58c14f25c7f533c96b134e3fe157da53d
Dhanyajeyakumarjk <dhanyadhanyajk@gmail>
2024-02-06 20:46:32 +0530

    first

--- divisible.java
+++ divisible.java
@@ -1,6 +1,17 @@
+import java.util.*;
 public class divisible {
     public static void main(String[] args){
-        Scanner s=new Scanner(System)
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the value:");
+        int a=s.nextInt();
+        
+        if(a%5==0 || a%11==0){
+            System.out.println("the number" +" "+a+" "+"is divisible by 5 and 11 ");
+        }
+        else{
+            System.out.println("the number" +" "+a +" "+"is not divisible by 5 and 11 ");
+            
+        }
     }
     
 }
