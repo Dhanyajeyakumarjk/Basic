bc3d31f50cb79dcf27a55f0bf50c42dd3be97bd5
Dhanyajeyakumarjk <dhanyadhanyajk@gmail>
2024-02-07 09:51:57 +0530

    firstchange

--- divisible.java
+++ divisible.java
@@ -5,11 +5,11 @@ public class divisible {
         System.out.println("Enter the value:");
         int a=s.nextInt();
         
-        if(a%5==0 || a%11==0){
-            System.out.println("the number" +" "+a+" "+"is divisible by 5 and 11 ");
+        if(a%8==0 || a%4==0){
+            System.out.println("the number" +" "+a+" "+"is divisible by 8 and 4");
         }
         else{
-            System.out.println("the number" +" "+a +" "+"is not divisible by 5 and 11 ");
+            System.out.println("the number" +" "+a +" "+"is not divisible by 8 and 4 ");
             
         }
     }
