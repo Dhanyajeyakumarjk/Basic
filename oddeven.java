680cc7435111f8ec06f0dec3ca85d311a6740bbb
Dhanyajeyakumarjk <dhanyadhanyajk@gmail>
2024-02-07 10:27:43 +0530

    first

---
+++ oddeven.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class oddeven{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int a=s.nextInt();
+        if(a%2==0){
+            System.out.println("number is even");
+        
+        }
+        else{
+            System.out.println("number is odd ");
+        }
+
+
+    }
+    
+}
