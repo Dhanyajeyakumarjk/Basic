cdcefb0b84e96aa73097a0c070fff4e4b68d2c26
Dhanyajeyakumarjk <dhanyadhanyajk@gmail>
2024-02-07 11:44:56 +0530

    first

---
+++ leapyear.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class leapyear
+{
+    public static void main (String [] args)
+    {
+        Scanner s1= new Scanner(System.in);
+        System.out.println("enter a year: ");
+        int a= s1.nextInt();
+       
+        if(a%4==0 && a%100!=0)//divisible by 4
+        {
+            System.out.println("Leap year");
+        }
+        else
+        {
+            System.out.println("Not leap year");
+        }
+    }
+}
