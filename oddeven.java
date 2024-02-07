591be6f4162d30f4d6a8b70db119c78d1e514f28
Dhanyajeyakumarjk <dhanyadhanyajk@gmail>
2024-02-07 10:29:24 +0530

    firstchange

--- oddeven.java
+++ oddeven.java
@@ -1,9 +1,10 @@
 import java.util.*;
 public class oddeven{
     public static void main(String[] args){
-        Scanner s=new Scanner(System.in);
-        System.out.println("Enter a number:");
-        int a=s.nextInt();
+        int a=22;
+        //Scanner s=new Scanner(System.in);
+        System.out.println("Enter a number:"+a);
+        //int a=s.nextInt();
         if(a%2==0){
             System.out.println("number is even");
         
