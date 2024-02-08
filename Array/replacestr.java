a079a59eadd8d78ad6bf39aa38f8c18a852643d6
Dhanyajeyakumarjk <dhanyadhanyajk@gmail>
2024-02-08 16:04:10 +0530

    f

---
+++ Array/replacestr.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class replacestr {
+    
+ 
+    
+        public static void main(String[] args)
+        {
+            String s1= new String("welcome to java programming");
+            System.out.print(s1.replace("java","python"));
+        }
+    }
+    //replace
---
+++ Array/secondlargest.java
@@ -0,0 +1,27 @@
+package Array;
+//Write a Java program to find the second largest element in an array.
+public class secondlargest
+{
+    public static void main(String[] args)
+    {
+        int a[]={12,23,34,45,5,10};
+        for(int i=0; i<a.length; i++)
+        {
+            for(int j=i+1; j<a.length; j++)
+            {
+                if(a[i]>a[j])
+                {
+                    int temp= a[i];
+                    a[i]= a[j];
+                    a[j]= temp;
+                }
+            }
+        }
+        for(int i=0; i<a.length;i++)
+        {
+            System.out.print(a[i]+" ");
+        }
+        System.out.println();
+        System.out.print(a[a.length-2]);
+    }
+}
