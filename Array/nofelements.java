68e7aa8adbe3cc0be04f236e0796920c1a2054a6
Dhanyajeyakumarjk <dhanyadhanyajk@gmail>
2024-02-08 15:46:54 +0530

    f

---
+++ Array/nofelements.java
@@ -0,0 +1,11 @@
+package Array;
+
+//Find the number of elements in the given array
+public class nofelements
+{
+    public static void main(String[] args)
+    {
+        int a[]={12,23,34,45,5,10};
+        System.out.print(a.length);
+    }
+}
