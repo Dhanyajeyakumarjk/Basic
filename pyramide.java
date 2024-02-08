94dd3b9eae9f2313ca98343fdee597deb56b5284
Dhanyajeyakumarjk <dhanyadhanyajk@gmail>
2024-02-08 11:17:34 +0530

    sss

--- pyramide.java
+++ pyramide.java
@@ -1,20 +1,21 @@
-/*public class pyramide
+public class pyramide
 {
     public static void main (String [] args)
     {
         char ch='a';
         for(int i=1; i<=5; i++)
         {
-            for(int j=1; j<=; j++)
+            for(int j=1; j<=i; j++)
             {
-                System.out.print(i+" ");
+                System.out.print(ch+" ");
                 ch++;
             }
             System.out.println();
         }
     }
-}*/
+}
 //number
+/* 
 public class pyramide
 {
     public static void main (String [] args)
@@ -30,4 +31,4 @@ public class pyramide
             System.out.println();
         }
     }
-}
+}*/
