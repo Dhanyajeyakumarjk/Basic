8144b1891bd9497dc379277af598cf41cdebcade
Dhanyajeyakumarjk <dhanyadhanyajk@gmail>
2024-02-08 12:03:00 +0530

    f

---
+++ Array/arr.java
@@ -0,0 +1,21 @@
+package Array;
+
+/*public class arr
+{
+    public static void main(String[] args)
+    {
+        String a[]= {"Lotus","Lily","Daisy","Jasmine"};
+        for(int i=0; i<a.length; i++)
+            System.out.println(a[i]);
+    }
+}/* */
+// for each to print an array
+public class arr
+{
+    public static void main(String[] args)
+    {
+        String s1[]= {"Lotus","Lily","Daisy","Jasmine"};
+        for(String a:s1)
+            System.out.println(a);
+    }
+}
